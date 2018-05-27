efc1eb81ca6ba36481ef3e12400f493ebb7af46d
mengligexue <836992030@qq.com>
2018-05-27 16:32:58 +0800

    xiugai

--- Scripts/test001.py
+++ Scripts/test001.py
@@ -2,7 +2,12 @@ import pytest,allure
 
 class Test_001:
 
-    @allure.step(title='步骤说明')
+    @allure.step(title='步骤说明001')
     @pytest.mark.parametrize("a",[1,2,3])
     def test_adc(self,a):
         assert a!=2
+
+    @allure.step(title='步骤说明002')
+    @pytest.mark.parametrize("a",[2,3,4])
+    def test_bcd(self,a):
+        assert a==2
